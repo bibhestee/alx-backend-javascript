6c43c90c680ec22eefa0a72ffcec59666bdb3f13
Bibest <bibhestee@gmail.com>
2023-06-28 19:51:34 +0100

    return a promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,16 @@
 import utils from './utils';
 
 export default function handleProfileSignup() {
-  try {
-    const { uploadPhoto, createUser } = utils;
-    // Extract promise response from the functions
-    const uploadPhotoResponse = uploadPhoto();
-    const createUserResponse = createUser();
-    // Promise.all is used to handle multiple promise at a time
-    Promise.all([uploadPhotoResponse, createUserResponse])
-      .then(([response1, response2]) => {
-        const { body } = response1;
-        const { firstName, lastName } = response2;
-        console.log(`${body} ${firstName} ${lastName}`);
-      })
-      .catch((err) => console.log(err));
-  } catch (error) {
-    console.log('Signup system offline');
-  }
+  const { uploadPhoto, createUser } = utils;
+  // Extract promise response from the functions
+  const uploadPhotoResponse = uploadPhoto();
+  const createUserResponse = createUser();
+  // Promise.all is used to handle multiple promise at a time
+  return Promise.all([uploadPhotoResponse, createUserResponse])
+    .then(([response1, response2]) => {
+      const { body } = response1;
+      const { firstName, lastName } = response2;
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
