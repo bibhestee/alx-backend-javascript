a69bd0b5e55f1380a696b3062e4958dbc48291d4
Bibest <bibhestee@gmail.com>
2023-06-28 20:37:07 +0100

    Import functions separately

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,12 @@
-import utils from './utils';
+import uploadPhoto from './utils';
+import createUser from './utils';
 
 export default function handleProfileSignup() {
-  const { uploadPhoto, createUser } = utils;
   // Extract promise response from the functions
   const uploadPhotoResponse = uploadPhoto();
   const createUserResponse = createUser();
   // Promise.all is used to handle multiple promise at a time
-  return Promise.all([uploadPhotoResponse, createUserResponse])
+  Promise.all([uploadPhotoResponse, createUserResponse])
     .then(([response1, response2]) => {
       const { body } = response1;
       const { firstName, lastName } = response2;
