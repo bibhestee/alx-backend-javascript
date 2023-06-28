e7f19cca31f74152603fb3ed49b23a80f7152104
Bibest <bibhestee@gmail.com>
2023-06-28 12:24:51 +0100

    finally handler added

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,14 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then((resolve) => {
-    if (resolve == undefined) {
+  promise.then(() => {
       return {
         status: 200,
         body: 'success',
-      };
-    }
-
-    return new Error();
+      };    
   }).catch(() => {
+    return new Error();
+  }).finally(() => {
     console.log('Got a response from the API');
   });
 }
