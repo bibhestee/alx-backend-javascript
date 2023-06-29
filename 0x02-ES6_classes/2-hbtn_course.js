13f35b698739567ff6c9932e0481d4f07f8838b8
Bibest <bibhestee@gmail.com>
2023-06-29 13:24:02 +0100

    no-underscore-dangle rectified

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,28 +1,37 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
+    // eslint-disable-next-line no-underscore-dangle
     if (typeof (name) === 'string') { this._name = name; }
+    // eslint-disable-next-line no-underscore-dangle
     if (typeof (length) === 'number') { this._length = length; }
+    // eslint-disable-next-line no-underscore-dangle
     if (Array.isArray(students)) { this._students = students; }
   }
 
   // Getter and setter for name attribute
+  // eslint-disable-next-line no-underscore-dangle
   get name() { return this._name; }
 
   set name(value) {
+    // eslint-disable-next-line no-underscore-dangle
     if (typeof (value) !== 'string') { throw new TypeError('Name must be a string'); } else { this._name = value; }
   }
 
   // Getter and setter for length attribute
+  // eslint-disable-next-line no-underscore-dangle
   get length() { return this._length; }
 
   set length(value) {
+    // eslint-disable-next-line no-underscore-dangle
     if (typeof (value) !== 'number') { throw new TypeError('Length must be a number'); } else { this._length = value; }
   }
 
   // Getter and Setter for students attribute
+  // eslint-disable-next-line no-underscore-dangle
   get students() { return this._students; }
 
   set students(value) {
+    // eslint-disable-next-line no-underscore-dangle
     if (Array.isArray(value) === false) { throw new TypeError('Students must be an array'); } else { this._students = value; }
   }
 }
