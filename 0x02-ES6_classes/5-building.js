5be183963540b78f0ad5327d820be5717d617b8b
Bibest <bibhestee@gmail.com>
2023-07-02 18:27:31 +0100

    prevent extension without implementing a method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,6 +2,8 @@ export default class Building {
   constructor(sqft) {
     // eslint-disable-next-line no-underscore-dangle
     if (typeof sqft === 'number') { this._sqft = sqft; } else { throw new TypeError('SQFT must be a number'); }
+    // any class that extends from Building must implement a method named evacuationWarningMessage
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) { throw new Error('Class extending Building must override evacuationWarningMessage'); }
   }
 
   // Getter method for sqft
@@ -14,10 +16,5 @@ export default class Building {
   set sqft(value) {
     // eslint-disable-next-line no-underscore-dangle
     if (typeof value === 'number') { this._sqft = value; } else { throw new TypeError('SQFT must be a number'); }
-  }
-
-  evacuationWarningMessage() {
-    // eslint-disable-next-line class-methods-use-this
-    if (new.target === Building) { throw new Error('Class extending Building must override evacuationWarningMessage'); }
   }
 }
