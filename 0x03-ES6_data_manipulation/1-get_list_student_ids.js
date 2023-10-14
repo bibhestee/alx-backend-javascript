fde8a567e7b389454db201c24da1ac2af99c8c23
Bibest <Bibhestee@gmail.com>
2023-10-14 18:26:37 +0000

    null argument implemented

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,6 @@
 export default function getListStudentIds(students) {
   if (typeof students !== 'object') return [];
+  if (!students) return [];
   const ids = students.map((student) => (student.id));
   return ids;
 }
