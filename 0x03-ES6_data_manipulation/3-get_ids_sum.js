4b283389bb88231525e8f58702b54fe82434d7e4
Bibest <Bibhestee@gmail.com>
2023-10-14 19:36:42 +0000

    fix lint issue

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(students) {
   if (typeof students !== 'object') return [];
   if (!students) return [];
-  const idsSum = students.reduce((total, current) => ( total+ current.id), 0);
+  const idsSum = students.reduce((total, current) => (total + current.id), 0);
   return idsSum;
 }
