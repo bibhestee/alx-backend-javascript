6dd79ae13944e007ba1bcc7dff2d55da6b2cadcc
Bibest <bibhestee@gmail.com>
2023-11-21 15:56:29 +0100

    create a simple http server with http module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/*
+ * In a file named 4-http.js, create a small HTTP server using the http module:
+
+ * It should be assigned to the variable app and this one must be exported
+ * HTTP server should listen on port 1245
+ * Displays Hello Holberton School! in the page body for any endpoint as plain text
+* */
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245, '0.0.0.0', () => { console.log('running'); });
