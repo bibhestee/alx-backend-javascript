69ac34e7f03a146e51e9dc9e6e9b903862f31ca7
Bibest <bibhestee@gmail.com>
2023-11-21 13:20:37 +0100

    read file synchronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,7 +5,7 @@
  * Create a function named countStudents. It should accept a path in argument
  * The script should attempt to read the database file synchronously
  *  If the database is not available, it should throw an error with the text
-      Cannot load the database
+      (Cannot load the database)
  *  If the database is available, it should log the following message to the
       console Number of students: NUMBER_OF_STUDENTS
  *  It should log the number of students in each field, and the list with the following format:
