94399138eb6439d588267715c81825c7ba39903b
Bibest <bibhestee@gmail.com>
2023-11-21 16:01:58 +0100

    make the response plain text

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,12 +7,11 @@
  * Displays Hello Holberton School! in the page body for any endpoint as plain text
 * */
 const http = require('http');
-const port = 1245;
-const host = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
-app.listen(port, hostname, () => { console.log('running'); });
+app.listen(1245, '0.0.0.0', () => { console.log('running'); });
