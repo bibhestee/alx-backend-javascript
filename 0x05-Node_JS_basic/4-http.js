fb51d6f018cc05eef4ef2daa155ef6f3f41e82b5
Bibest <bibhestee@gmail.com>
2023-11-21 15:58:42 +0100

    create a simple http server with http module

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -7,10 +7,12 @@
  * Displays Hello Holberton School! in the page body for any endpoint as plain text
 * */
 const http = require('http');
+const port = 1245;
+const host = '127.0.0.1';
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.end('Hello Holberton School!');
 });
 
-app.listen(1245, '0.0.0.0', () => { console.log('running'); });
+app.listen(port, hostname, () => { console.log('running'); });
