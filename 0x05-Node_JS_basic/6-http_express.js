3f448695a6099fa5c4fba5fb308fcac2164970b9
Abdulbasit Abdullahi <Bibhestee@gmail.com>
2023-11-21 19:57:44 +0100

    Create simple http Server using express js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+/**
+  * Install Express and in a file named 6-http_express.js,
+     create a small HTTP server using Express module:
+  * It should be assigned to the variable app and
+     this one must be exported
+  * HTTP server should listen on port 1245
+  * Displays Hello Holberton School! in the page body
+     for the endpoint /
+  */
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+})
+
+app.listen(1245, () => {
+  console.log('app is running');
+})
+
+module.exports = app;
