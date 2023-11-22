6e26ff8e1070d20f9f4ae88a6bd7813409406a2e
Bibest <bibhestee@gmail.com>
2023-11-22 17:08:56 +0100

    change assertion to expect from chai library

--- 0x06-unittets_in_js/0-calcul.test.js
+++ 0x06-unittets_in_js/0-calcul.test.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
 describe('calculate Number', () => {
-    it('should return 5 when the 1.5 and 3.7 is passed', () => {
-      assert.equal(calculateNumber(1.5, 3.7), 6);
-    });
+  it('should return 5 when the 1.5 and 3.7 is passed', () => {
+    assert.equal(calculateNumber(1.5, 3.7), 6);
+  });
 });
